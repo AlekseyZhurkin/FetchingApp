66305f040176a1f5ad7ddfdc9b6a35d0ffa37d0f
Aleksey Zhurkin <web.zhurkin@gmail.com>
2025-01-11 17:31:40 +0300

    Initial Commit

---
+++ FetchingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchingApp
+//
+//  Created by Алексей Журкин on 11.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
